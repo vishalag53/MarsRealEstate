e8a441d7a727746b0b886e3f4b8da3c2b6fc855e
vishalag53 <vishal.agrawal1207@gmail.com>
2023-07-07 21:35:24 +0530

    Step 2 Parsing JSON with Moshi

--- app/src/main/java/com/vishalag53/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/vishalag53/marsrealestate/network/MarsProperty.kt
@@ -1,3 +1,10 @@
 package com.vishalag53.marsrealestate.network
 
-class MarsProperty()
+import com.squareup.moshi.Json
+
+data class MarsProperty(
+    val id: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    val price: Double
+)
--- app/src/main/java/com/vishalag53/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/vishalag53/marsrealestate/overview/OverviewViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.vishalag53.marsrealestate.network.MarsApi
+import com.vishalag53.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Response
 
@@ -19,12 +20,12 @@ class OverviewViewModel: ViewModel() {
     }
 
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(object : retrofit2.Callback<String>{
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
+        MarsApi.retrofitService.getProperties().enqueue(object : retrofit2.Callback<List<MarsProperty>>{
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                 _response.value = "Failure: " + t.message
             }
         })
